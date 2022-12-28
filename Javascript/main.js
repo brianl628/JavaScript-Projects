54fe26618ddc272fdafd9bbc4d110e074cd319d9
brianl628 <118641307+brianl628@users.noreply.github.com>
2022-12-27 20:20:01 -0500

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,9 @@
+function My_First_Function() {              //Defining function and giving it a name
+    var str = "This is the button text!";   //Defining variable
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+window.alert("Hello, world!")
+
+document.write("This is the beginning of the string" //Concatenate two strings
++ " and this is the end of the string.")
