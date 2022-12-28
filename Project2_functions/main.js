41158f52f7f9b27c44b29dbd704f20d37cbd0515
brianl628 <118641307+brianl628@users.noreply.github.com>
2022-12-27 20:30:55 -0500

    Add files via upload

---
+++ Project2_functions/main.js
@@ -0,0 +1,5 @@
+function myFunction() {         //Defining myFunction
+    var sentence = "I am learning"; 
+    sentence += " a lot from this book!";    //Concatenate and using operator 
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
