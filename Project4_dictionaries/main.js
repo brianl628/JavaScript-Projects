710c135cb7e315a13185c7d1ce23c734146d084a
brianl628 <118641307+brianl628@users.noreply.github.com>
2022-12-28 19:10:00 -0600

    Update main.js

--- Project4_dictionaries/main.js
+++ Project4_dictionaries/main.js
@@ -6,6 +6,6 @@ function my_Dictionary() {      //Dictionary function
         Age:5,
         Sound:"Bark!"
     };
-    delete Animal.Species;        //Delete operator
-    document.getElementById("Dictionary").innerHTML = Animal.Species;   //Displays animal species
+    delete Animal.Sound;        //Delete operator
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;   //Displays animal species
 }
