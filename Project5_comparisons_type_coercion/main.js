6e442c848d975c5217026f367206140d4618b6ec
brianl628 <118641307+brianl628@users.noreply.github.com>
2022-12-29 19:57:32 -0500

    Add files via upload

---
+++ Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,21 @@
+document.write (typeof "Word") //Defines variable
+
+document.write ("10" + 5); //Combines string and number
+
+document.write(10 > 2); //greater than operation
+
+document.write(10 < 2); //less than operation
+
+document.write(10 == 10);
+
+X = 11;
+Y = 12;
+document.write (X === Y); //compares the two defined variables
+
+document.write(5 > 10 || 10 > 4);
+document.write(5 > 2 && 10 > 4);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
